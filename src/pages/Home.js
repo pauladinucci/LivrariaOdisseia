ae9b681edb03ad03d30798b7f3c1cccc744722d9
letdayy <lcdayane49@gmail.com>
2022-12-07 14:13:27 -0300

    conserto dos bugs

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,6 @@
-<<<<<<< HEAD
 import Carousel from "./Inicio/Carousel";
 import './Home.css';
 import CardInicio from "./Inicio/Card";
-
-
 
 const Home = () => {
     return (
@@ -27,36 +24,4 @@ const Home = () => {
         </>
     )
 };
-
-=======
-import Carousel from "./Inicio/Carousel";
-import './Home.css';
-import CardInicio from "./Inicio/Card";
-
-
-
-const Home = () => {
-    return (
-        <>
-            <Carousel />
-            <h1>Mangás</h1>
-            <div className="Cards">
-                <CardInicio />
-                <CardInicio />
-                <CardInicio />
-                <CardInicio />
-            </div>
-
-            <h1>Autoajuda</h1>
-            <div className="Cards">
-                <CardInicio />
-                <CardInicio />
-                <CardInicio />
-                <CardInicio />
-            </div>
-        </>
-    )
-};
-
->>>>>>> letdayy
 export default Home;
