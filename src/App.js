58d01b0aaf9499513304948ebb38439f7372c908
Sergio Luiz Dos Santos Alves <113436222+sergio22ko@users.noreply.github.com>
2022-12-08 09:04:14 -0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,24 +1,86 @@
-import logo from './logo.svg';
-import './App.css';
+import './style.css'
+import { useState } from "react";
 
 function App() {
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="container-login">
+        <div className="wrap-login">
+          <form className="login-form">
+
+            <FormGroup className="position-relative">
+              <Label for="exampleEmail">
+                Valid input
+    </Label>
+              <Input valid />
+              <FormFeedback
+                tooltip
+                valid
+              >
+                Sweet! that name is available
+    </FormFeedback>
+              <FormText>
+                Example help text that remains unchanged.
+    </FormText>
+            </FormGroup>
+            <FormGroup className="position-relative">
+              <Label for="examplePassword">
+                Invalid input
+    </Label>
+              <Input invalid />
+              <FormFeedback tooltip>
+                Oh noes! that name is already taken
+    </FormFeedback>
+              <FormText>
+                Example help text that remains unchanged.
+    </FormText>
+            </FormGroup>
+
+            <span className="login-form-title">Bem Vindo!</span>
+
+            <span className="login-form-title">
+
+            </span>
+
+            <div className="wrap-input">
+              <input
+                className="input"
+                type="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+              <span className="focus-input" data-placeholder="Email"></span>
+            </div>
+
+            <div className="wrap-input">
+              <input
+                className="input"
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              <span className="focus-input" data-placeholder="Password"></span>
+            </div>
+
+            <div className="container-login-form-btn">
+              <button className="login-form-btn">Login</button>
+            </div>
+
+            <div className="text-center">
+              <span className="txt1">Não possui conta?</span>
+              <a className="txt2" href="#">Criar conta.</a>
+            </div>
+
+
+          </form>
+        </div>
+      </div>
     </div>
+
   );
 }
 
